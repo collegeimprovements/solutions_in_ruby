19260745df9ff50c766b5bde9ca400db85cbb7c7
collegeimprovements <collegeimprovements@gmail.com>
2013-06-21 20:31:06 +0530

    half solved currency-problem

--- currency name.rb
+++
@@ -1,4 +0,0 @@
-puts "Enter the number : "
-no = gets
-
-(1..no).each {|no| "#{fizz if no %3.zero?}#{buzz if no% 5.zero?}#{no % 3 !=0 && no % 5 != 0}"} 
---
+++ currency.rb
@@ -0,0 +1,12 @@
+val = ['zero','one','two','three','four','five','six','seven','eight','nine']
+cur = ['one','ten','hundred','thousand','ten-thousand','lac','ten-lacs','crore']
+
+num = 52413
+cur_arr = (0..((num.to_s.length)-1)).to_a.reverse  #[*0..4].reverse 
+
+no = num.to_s.split("").map(&:to_i)
+
+no.each {|i| puts val[i]}
+
+
+cur_arr.each {|j| puts cur[j] }
